177dd9b427db51f8b7c6f764b23284aaf435eaff
Arun <d.aarun@yahoo.in>
2020-12-13 23:06:17 +0530

    Added routes for forgot password

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,6 +11,7 @@ const cryptoRandomString = require("crypto-random-string");
 const bcrypt = require('bcryptjs');
 const nodemailer = require("nodemailer");
 const config = require("config");
+const { async } = require('crypto-random-string');
 
 let transporter = nodemailer.createTransport({
   host: 'smtp.ethereal.email',
@@ -176,7 +177,7 @@ router.get(
   }
 );
 
-router.post('/editDetails', authenticate.verifyUser, async (req, res, next) => {
+router.put('/editDetails', authenticate.verifyUser, async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id);
     if (req.body.firstname)
@@ -197,6 +198,72 @@ router.post('/editDetails', authenticate.verifyUser, async (req, res, next) => {
     res.json({success: true, status: 'Details Succesfully Updated!', user: user});
   }
   catch (err) {
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: false, err: err}); 
+  }
+});
+
+router.post('/password/forgot', async (req, res) =>{
+  try {
+    const user = await User.findOne({email: req.body.email});
+
+    if (!user) {
+        res.json({ success: false });
+    } else if(user.status !== "verified"){
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: false, status: 'Email not verifiedl!'});
+    } else {
+        await verifyCode.deleteMany({ email: user.email });
+
+        const secretCode = cryptoRandomString({
+          length: 6,
+        });
+        const newCode = new verifyCode({
+          code: secretCode,
+          email: req.body.email,
+        });
+        await newCode.save();
+
+        const data = {
+          from: `OneStopApp`,
+          to: user.email,
+          subject: "Your Password reset code for OneStopApp",
+          text: `Please use the following code to reset your password: ${secretCode}`,
+        };
+        await transporter.sendMail(data);
+
+        res.json({ success: true });
+    }
+  } catch (err) {
+    res.statusCode = 401;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: false, err: err}); 
+  }
+});
+
+router.post("/password/change", async (req, res) => {
+  try {
+    const response = await verifyCode.findOne({
+      email: req.body.email,
+      code: req.body.secretCode,
+    });
+
+    if (!response) {
+      res.sendStatus(401);
+    } else {
+      const user = await User.findOne({email: req.body.email});
+      await user.setPassword(req.body.password);
+      await user.save();
+
+      await verifyCode.deleteMany({ email: user.email });
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({success: true, status: 'Password Changed!'});
+    }
+  } catch (err) {
     res.statusCode = 401;
     res.setHeader('Content-Type', 'application/json');
     res.json({success: false, err: err}); 
